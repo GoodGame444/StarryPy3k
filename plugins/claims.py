6b9f4d58ee82f17c6bd795562d5f892f97bbe4e9
FezzedOne <RenoFezzedOne@yandex.com>
2025-03-19 11:55:48 -0400

    Another clarification.

--- plugins/claims.py
+++ plugins/claims.py
@@ -82,7 +82,8 @@ class Claims(StorageCommandPlugin):
                         # to avoid a known bug where the claim sometimes gets assigned to a different
                         # character/player with the same alias (but a different discriminator).
                         # This comes at the cost of resetting the list of allowed builders on any
-                        # shipworld to «only the owner» every time the owner rejoins.
+                        # shipworld to «only the owner» every time the owner rejoins or a shipworld
+                        # is (re-)loaded.
                         self.planet_protect.disable_protection(ship)
                         self.planet_protect.add_protection(ship,
                                                             connection.player)
